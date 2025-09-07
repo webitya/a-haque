d0ecbb32045b29baf5257892e81164c90a18a48a
Aditya Kumar <webitya@gmail.com>
2025-09-07 14:32:59 +0530

    Aditya

--- src/components/homepage/Footer.js
+++ src/components/homepage/Footer.js
@@ -38,7 +38,7 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-300">
-          <p>&copy; 2024 Aastha Ortho & Neurotherapy Center. All rights reserved.</p>
+          <p>&copy; 2025 Aastha Ortho & Neurotherapy Center. All rights reserved.</p>
         </div>
       </div>
     </footer>
