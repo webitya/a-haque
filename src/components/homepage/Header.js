72294639a0eedc534ab4c826bb67f8cc633da1f8
Aditya Kumar <webitya@gmail.com>
2025-09-07 11:21:36 +0530

    Aditya

--- src/components/homepage/Header.js
+++ src/components/homepage/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
   return (
     <>
       <header className="bg-white shadow-sm border-b border-border sticky top-0 z-50">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto px-4 sm:px-4 lg:px-6">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
